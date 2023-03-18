ad7dfdf831426dce741dac1b36c155beb3d5fd6e
emilioeiji <emilioeiji@gmail.com>
2023-03-18 14:30:55 +0900

    Ajustando a tela modal.

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
 
+import commonStyles from '../commonStyles'
 export default class AddTask extends Component {
     render() {
         return(
@@ -10,6 +11,16 @@ export default class AddTask extends Component {
                 onRequestClose={this.props.onCancel}
                 animationType='slide'
             >
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}
+                >
+                    <View style={style.background}>
+
+                    </View>
+                </TouchableWithoutFeedback>
+                <View style={style.container}>
+                    <Text style={style.header}>Nova Tarefa</Text>
+                </View>
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}
                 >
@@ -27,4 +38,16 @@ const style = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)',
     },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18,
+    }
 })
